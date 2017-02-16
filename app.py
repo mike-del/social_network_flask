8c813f265dbc39a4d4988855d461c24f69cc8d7c
Michael Delgado <intrepidcreativity@gmail.com>
2017-02-16 14:59:17 -0800

    returned to correct python 3 code for post timestamp/ need to adjust for heroku in future 1

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import datetime, time
 from flask import (Flask, g, render_template, flash, redirect, url_for,
                     abort)
 from flask.ext.bcrypt import check_password_hash
 from flask.ext.login import (LoginManager, login_user, logout_user,
                             login_required, current_user)
-
 import forms
 import models
 
